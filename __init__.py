354b6ac5bc2836761bb51d74942a17c8bb2083c6
sw55555 <snows55555@hotmail.com>
2025-11-17 19:05:25 +0800

    fix: LoadImageByUrl not export

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from .byte_plus_v2 import CxBytePlus2ImageV2
 from .byte_plus_v2 import CxBytePlus2VideoV2
+from .load_image_by_url import LoadImageByUrl
 from .image_scale_by_aspect_ratio_v2 import ImageScaleByAspectRatioV2
 
 NODE_CLASS_MAPPINGS = {
+    "LoadImageByUrl": LoadImageByUrl,
     "CxBytePlus2Image": CxBytePlus2ImageV2,
     "CxBytePlus2Video": CxBytePlus2VideoV2,
     "LayerUtility: ImageScaleByAspectRatio V2": ImageScaleByAspectRatioV2

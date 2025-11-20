194c929ccdd2713827961951c7d9450a8f3abd84
vscode <vscode@qq.com>
2025-11-20 12:10:32 +0000

    ADD GeneralReversedSwitch

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from .impact.util_nodes import GeneralReversedSwitch
 from .byte_plus_v2 import CxBytePlus2ImageV2
 from .byte_plus_v2 import CxBytePlus2VideoV2
 from .load_image_by_url import LoadImageByUrl
@@ -5,6 +6,7 @@ from .image_scale_by_aspect_ratio_v2 import ImageScaleByAspectRatioV2
 
 NODE_CLASS_MAPPINGS = {
     "LoadImageByUrl": LoadImageByUrl,
+    "GeneralReversedSwitch": GeneralReversedSwitch,
     "CxBytePlus2Image": CxBytePlus2ImageV2,
     "CxBytePlus2Video": CxBytePlus2VideoV2,
     "LayerUtility: ImageScaleByAspectRatio V2": ImageScaleByAspectRatioV2
---
+++ impact/util_nodes.py
@@ -0,0 +1,48 @@
+from .utils import any_typ, ByPassTypeTuple, is_execution_model_version_supported
+import logging
+
+
+class GeneralReversedSwitch:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "select": ("INT", {"default": 1, "min": 1, "max": 999999, "step": 1,
+                                   "tooltip": "The output number you want to send from the input"}),
+                "input": (any_typ, {"tooltip": "Any input. When connected, one more input slot is added."}),
+            },
+        }
+
+    RETURN_TYPES = ByPassTypeTuple((any_typ,))
+    OUTPUT_TOOLTIPS = (
+        "Output occurs only from the output selected by the 'select' value.\nWhen slots are connected, additional slots are created.",)
+    FUNCTION = "doit"
+
+    CATEGORY = "cx/GeneralReversedSwitch"
+
+    def doit(self, select, prompt, unique_id, input, **kwargs):
+        if is_execution_model_version_supported():
+            from comfy_execution.graph import ExecutionBlocker
+        else:
+            logging.warning(
+                "[Impact Pack] InversedSwitch: ComfyUI is outdated. The 'select_on_execution' mode cannot function properly.")
+
+        res = []
+
+        # search max output count in prompt
+        cnt = 0
+        for x in prompt.values():
+            for y in x.get('inputs', {}).values():
+                if isinstance(y, list) and len(y) == 2:
+                    if y[0] == unique_id:
+                        cnt = max(cnt, y[1])
+
+        for i in range(0, cnt + 1):
+            if select == i + 1:
+                res.append(input)
+            elif is_execution_model_version_supported():
+                res.append(ExecutionBlocker(None))
+            else:
+                res.append(None)
+
+        return res
